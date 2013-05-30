28e6bbbff538430a013d89c51d4c571a8508c021
Kim Thoenen <kim@smuzey.ch>
2013-05-30 14:01:33 +0200

    a try

--- src/dakitoto/tictactoe/gui/gui.java
+++ src/dakitoto/tictactoe/gui/gui.java
@@ -1,8 +1,16 @@
 package dakitoto.tictactoe.gui;
 
-public class gui {
+import java.awt.Container;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+import javax.swing.*;
+
+public class gui extends JFrame {
 	
-	// test
+	// test 2
 	
 	
 	
@@ -10,7 +18,40 @@ public class gui {
 	
 	
 	public gui(){
+		setTitle("My Empty Frame");
+		setSize(300,200); // default size is 0,0
+		setLocation(10,200); // default is 0,0 (top left corner)
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
+		// Window Listeners
+		addWindowListener(new WindowAdapter() {
+		  public void windowClosing(WindowEvent e) {
+			System.exit(0);
+		  } //windowClosing
+		}); //addWindowLister
+	
+		// Add Panels
+		Container contentPane = getContentPane();
+		
+		JButton button = new JButton("test");
+        //Add action listener to button
+        button.addActionListener(new ActionListener() {
+ 
+            public void actionPerformed(ActionEvent e)
+            {
+                //Execute when button is pressed
+                System.out.println("You clicked the button");
+            }
+        }); 
+ 
+        getContentPane().add(button);
+        pack();
+        setVisible(true);
 		
 	}
+	
+	public static void main(String[] args) {
+	    JFrame f = new gui();
+	    f.show();
+	  }
 }
